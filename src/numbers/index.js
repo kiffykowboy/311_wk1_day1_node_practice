a7649ebf8355246053afac04f1ac12818faca750
kiffykowboy <chris@Chriss-MacBook-Pro.local>
2021-07-01 22:48:08 -0500

    tests passing

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,26 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if (num % 2 == 0){
+    return true
+  }
+else {return false}
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  let sum = arr.reduce((a,b) => a + b, 0)
+  return sum
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  let inner = arr.concat(arr[0]);
+  for (let i = 0, {length} = inner; i < length; i++) {
+    for (let j = i + 1, {length} = inner; j < length; j++) {
+      if (inner[i] + inner[j] === sum) return true;
+    }
+  }
+  return false;
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,18 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  str = 'a-new-string'
+  const newStr = str.split("-");
+ return newStr
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+ return str.match(/(..?)/g)
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  return str.split("").reverse().join("");
 }
 
 module.exports = {
